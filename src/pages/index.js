e6d1d51c3c4548e2b4d35337041b53ea488d8dc3
unknown <anantdhok16@gmail.com>
2021-07-01 00:48:35 +0530

    Update all functionalities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,38 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Cookies from "js-cookie";
 import { FaGithub, FaBehanceSquare, FaMedium } from "react-icons/fa";
+import { Data } from "../shared/data";
 import styles from "../shared/styles/home.module.scss";
 
 export default function Home(theme) {
-  const [click, setClick] = useState();
-  const handleClick = () => {
-    Cookies.set("aboutVisit", "checked", { expires: 1 });
-    setClick(!click);
+  const message = [
+    "I hope you're having a great day!",
+    "Welcome back, you missed anything?",
+    "Woah! Thanks for visiting again.",
+    "I think we're gonna be good friends!",
+    "You really like me, don't you :)",
+  ];
+  const change = [
+    "Ahh! I hope you've washed your hands.",
+    "Make yourself comfortable around!"
+  ];
+
+  const [count, setCount] = useState(!Cookies.get("viewCounts") ? 0 : +(Cookies.get("viewCounts")));
+  const [check, setCheck] = useState(false);
+  const [greet, setGreet] = useState(message[0]);
+  const resetGreet = () => {
+    setGreet(message[count]);
   };
 
   useEffect(() => {
-    if (Cookies.get("aboutVisit") !== "checked") setClick(true);
-    else setClick(false);
+    if (!check) {
+      let visits = +(Cookies.get("viewCounts"));
+      Cookies.set("viewCounts", visits > 3 ? 4 : count + 1, { expires: 1 });
+      setGreet(message[count]);
+      setCheck(true);
+    }
   });
 
   return (
@@ -26,46 +44,36 @@ export default function Home(theme) {
       </Head>
 
       <main className={styles.home}>
-        <section className="container" style={{ height: "80vh" }}>
+        <section className="container" onMouseEnter={resetGreet} onMouseLeave={() => setGreet(change[1])}>
           <div className={styles.intro}>
             <div className="text">
               <h1 className={styles.title}>Hi there, I'm Anant &#128075;</h1>
-              <p className={styles.description}>I hope you're having a great day!</p>
+              <p className={styles.description}>{greet}</p>
             </div>
-            <div className={styles.wrapper}>
-              <img src="/media/profile.png" alt="Anant Dhok" />
+            <div className={styles.wrapper} onMouseEnter={() => setGreet(change[0])} onMouseLeave={resetGreet}>
+              <Image className={styles.image} src="/media/profile.jpg" alt="Anant Dhok" layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
 
-        <section className="container">
+        <section className="container" style={{ paddingBottom: "22px" }}>
           <h2>Let me introduce myself</h2>
-          <p className={styles.paragraph}>
-            Myself Anant Dhok, an aspiring Full-Stack developer graduated from VIT, Pune — Proficient in C++, Python,
-            JavaScript(MERN Stack), and I'm also a self-taught Graphic Designing. I develop applications that people
-            enjoy using and create distinctive designs that grab people's attention.
-          </p>
+          <p className={styles.paragraph}>{Data.introduction}</p>
         </section>
 
         <section className="container">
           <h2>Explore</h2>
           <div className="grid">
-            <a href="https://github.com/anantdhok" className="card">
-              <h3>
-                <FaGithub className="icon" tyle={{ color: "black" }} /> Development
-              </h3>
+            <a href="https://github.com/anantdhok" className="card" target="_blank" rel="noopener noreferrer" >
+              <h3><FaGithub className="icon" tyle={{ color: "black" }} /> Development</h3>
               <p>Technical Projects, Assignments and much more.</p>
             </a>
-            <a href="https://www.behance.net/anantdhok" className="card">
-              <h3>
-                <FaBehanceSquare className="icon" style={{ color: "blue" }} /> Design
-              </h3>
+            <a href="https://www.behance.net/anantdhok" className="card" target="_blank" rel="noopener noreferrer" >
+              <h3><FaBehanceSquare className="icon" style={{ color: "blue" }} /> Design</h3>
               <p>Creative Projects, Brandings, Illustrations and much more.</p>
             </a>
-            <a href="https://medium.com/@anantdhok/about" className="card">
-              <h3>
-                <FaMedium className="icon" style={{ color: "orange" }} /> Writings
-              </h3>
+            <a href="https://medium.com/@anantdhok/about" className="card" target="_blank" rel="noopener noreferrer" >
+              <h3><FaMedium className="icon" style={{ color: "orange" }} /> Writings</h3>
               <p>Blogs related to Programming, Development and Design.</p>
             </a>
           </div>
@@ -74,81 +82,43 @@ export default function Home(theme) {
         <section className="container">
           <h2>Experiences</h2>
           <div className="grid">
-            <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
-              <h3>
-                Nexloop India <code className="code">Internship</code>
-              </h3>
-              <p>Full Stack Developer — 2020</p>
-            </a>
-            <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
-              <h3>
-                AstronEra <code className="code">Internship</code>
-              </h3>
-              <p>Graphic Designer — 2019</p>
-            </a>
-            <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
-              <h3>
-                TEDxVITPune <code className="code">Freelancing</code>
-              </h3>
-              <p>Head Designer — 2019-2021</p>
-            </a>
+            {Data.experiences.map((item) => (
+              <a href={item.link} className="card" target="_blank" rel="noopener noreferrer" >
+                <h3>
+                  <span className="icon"><Image src={item.src} alt="Anant Dhok" width={item.size} height={item.size} layout="fixed" /></span>
+                  {item.title} <code className="code">{item.subtitle}</code>
+                </h3>
+                <p>{item.description}</p>
+              </a>
+            ))}
           </div>
         </section>
 
         <section className="container">
           <h2>Academic Projects</h2>
           <div className="grid">
-            <a href="https://github.com/anantdhok" className="card">
-              <h3>RTEA</h3>
-              <p>Real-Time Emotion Analysis during physiological therapies.</p>
-              <span>
-                <li>&bull; Emotion Detection</li>
-                <li>&bull; Machine Learning</li>
-                <li>&bull; AWS</li>
-                <li>&bull; Socket</li>
-                <li>&bull; Web Development</li>
-              </span>
-            </a>
-            <a href="https://github.com/anantdhok" className="card">
-              <h3>SWIR</h3>
-              <p>Detection and Reconstruction model for radiometric errors.</p>
-              <span>
-                <li>&bull; Radiometric Errors</li>
-                <li>&bull; Machine Learning</li>
-                <li>&bull; Algorithm</li>
-                <li>&bull; Web Development</li>
-              </span>
-            </a>
-            <a href="https://github.com/anantdhok" className="card">
-              <h3>UDHR</h3>
-              <p>Unified Repository to store summarized health related reports.</p>
-              <span>
-                <li>&bull; Healthcare</li>
-                <li>&bull; Blockchain</li>
-                <li>&bull; NLP</li>
-                <li>&bull; Web Development</li>
-              </span>
-            </a>
-            <a href="https://github.com/anantdhok" className="card">
-              <h3>AnantMoney</h3>
-              <p>Application to implement crytocurrency within a small campus.</p>
-              <span>
-                <li>&bull; Crytocurreny</li>
-                <li>&bull; Blockchain</li>
-                <li>&bull; Web Development</li>
-              </span>
-            </a>
+            {Data.projects.map((item) => (
+              <a href={item.link} className="card" target="_blank" rel="noopener noreferrer" >
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <span>
+                  {item.tags.map((item) => (
+                    <li><em style={{ fontSize:"larger", color: item.color }}>&bull;</em> {item.title}</li>
+                  ))}
+                </span>
+              </a>
+            ))}
           </div>
         </section>
 
         <section className="container">
           <h2>Expertise</h2>
           <p className={styles.paragraph}>
-            <b>Skills: </b>Full Stack Development, Web Design/Development, Programming, UI/UX Design, Graphic Designing
-            <br />
-            <b>Tools: </b>C/C++ Programming Language, Python, JavaScript, PHP, HTML5, CSS3, SASS, Bootstrap, React.js,
-            Node.js, MySQL, MongoDB, Git, GitHub, Adobe Illustrator, Adobe InDesign, Adobe Photoshop, Figma, Microsoft
-            Office
+            <b>Skills: </b>
+            {Data.skills}
+            <br /><br />
+            <b>Tools: </b>
+            {Data.technologies}
           </p>
         </section>
       </main>
---
+++ src/shared/data/index.js
@@ -0,0 +1,75 @@
+export const Data = {
+  introduction:
+    "Myself Anant Dhok, an aspiring Full-Stack developer graduated from VIT, Pune — Proficient in C++, Python, JavaScript(MERN Stack), and I'm also a self-taught Graphic Designing. I develop applications that people enjoy using and create distinctive designs that grab people's attention.",
+  experiences: [
+    {
+      title: "Nexloop IN",
+      subtitle: "Internship",
+      description: "Full Stack Developer — 2020",
+      link: "https://nexloop.in",
+      src: "/media/icons/nl.svg",
+      size: 36
+    },
+    {
+      title: "AstronEra",
+      subtitle: "Internship",
+      description: "Graphic Designer — 2019",
+      link: "https://astronera.org/",
+      src: "/media/icons/ae.png",
+      size: 28    
+    },
+    {
+      title: "TEDxVITPune",
+      subtitle: "Freelancing",
+      description: "Head Designer — 2019-2021",
+      link: "https://tedxvitpune.in",
+      src: "/media/icons/tx.png",
+      size: 22
+    },
+  ],
+  projects: [
+    {
+      title: "RTEA",
+      description: "Real-Time Emotion Analysis during physiological therapies.",
+      link: "https://github.com/anantdhok",
+      tags: [
+        { color: "blue", title: "Emotion Detection" },
+        { color: "orangered", title: "ML" },
+        { color: "orange", title: "AWS" },
+        { color: "teal", title: "Socket" },
+      ],
+    },
+    {
+      title: "SWIR",
+      description: "Detection and Reconstruction model for radiometric errors.",
+      link: "https://github.com/anantdhok",
+      tags: [
+        { color: "blue", title: "Radiometric Errors" },
+        { color: "orangered", title: "ML" },
+        { color: "gold", title: "Algorithm" },
+      ],
+    },
+    {
+      title: "UDHR",
+      description: "Unified Repository to store summarized health related reports.",
+      link: "https://github.com/anantdhok",
+      tags: [
+        { color: "blue", title: "Healthcare" },
+        { color: "deeppink", title: "Blockchain" },
+        { color: "yellowgreen", title: "NLP" },
+      ],
+    },
+    {
+      title: "AnantMoney",
+      description: "Application to implement crytocurrency within a small campus.",
+      link: "https://github.com/anantdhok",
+      tags: [
+        { color: "blue", title: "Crytocurreny" },
+        { color: "deeppink", title: "Blockchain" },
+      ],
+    },
+  ],
+  skills: "Full Stack Development, Web Design/Development, Programming, UI/UX Design, Graphic Designing",
+  technologies:
+    "C/C++ Programming Language, Python, JavaScript, PHP, HTML5, CSS3, SASS, Bootstrap, React.js, Node.js, MySQL, MongoDB, Git, GitHub, Adobe Illustrator, Adobe InDesign, Adobe Photoshop, Figma, Microsoft Office",
+};
